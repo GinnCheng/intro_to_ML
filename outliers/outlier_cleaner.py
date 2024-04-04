2124fff607569bd192a6cbb8c1953217b157ba0e
ginn cheng <ginn.z.cheng@gmail.com>
2024-04-05 02:43:16 +0800

    chore: update outlier_cleaner Description: -the outlier_clearner function was updated to fix bugs -inputs are squeezed initially now -wrong argument 'ascend' was removed from np.argsort()

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -12,23 +12,27 @@ def outlierCleaner(predictions, ages, net_worths):
     """
     ### your code goes here
     import numpy as np
+    ## squeeze the array
+    predictions,ages,net_worths=np.squeeze(predictions),np.squeeze(ages),np.squeeze(net_worths)
     ## check if the three arrays are in the same length
     if (len(predictions)-len(ages))*(len(ages)-len(net_worths)) != 0:
         print('Error: the data lengths are not consistent')
         return None
     else:
         ## get the number of points that we need to remove
-        num_clean = int(np.round(len(predictions),0))
+        num_clean = int(np.round(0.1*len(predictions),0))
         ## get the index of the errors in a descending order
-        error_indx = np.argsort((net_worths - predictions)**2, ascend=True)[::-1]
+        errors = (net_worths - predictions)**2
+        error_indx = np.argsort(errors)[::-1]
         ## get the first num_clean of indices to be removed
         clean_indx = error_indx[:num_clean]
+        # print(f'sorted errors is {errors[clean_indx]}')
         ## clean the three arrays
-        cl_pred, cl_ages, cl_net = (np.delete(predictions, clean_indx),
-                                    np.delete(ages, clean_indx),
-                                    np.delete(net_worths, clean_indx))
+        cl_ages, cl_net, cl_error = (np.delete(ages, clean_indx),
+                                    np.delete(net_worths, clean_indx),
+                                     np.delete(errors, clean_indx))
 
-        cleaned_data = zip(cl_pred, cl_ages, cl_net)
+        cleaned_data = list(zip(cl_ages, cl_net, cl_error))
     
     return cleaned_data
 
