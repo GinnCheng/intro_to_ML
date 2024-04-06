7e8e8a9a8cbde15cec7abe68e0b02853cb754284
ginn cheng <ginn.z.cheng@gmail.com>
2024-04-07 02:14:21 +0800

    chore: filled in find_signature.py Description: -the main body of the script was filled in as per the quizzes

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -2,18 +2,20 @@
 
 import joblib
 import numpy
+import pickle
 numpy.random.seed(42)
 
 
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
+words_file = "../text_learning/your_word_data.pkl"
 authors_file = "../text_learning/your_email_authors.pkl"
-word_data = joblib.load( open(words_file, "r"))
-authors = joblib.load( open(authors_file, "r") )
-
-
+word_data = joblib.load( open(words_file, "rb"))
+authors = joblib.load( open(authors_file, "rb") )
+# word_data = pickle.load( open(words_file, 'rb'))
+# authors = pickle.load( open(authors_file, 'rb'))
+print(numpy.shape(word_data), numpy.shape(authors))
 
 ### test_size is the percentage of events assigned to the test set (the
 ### remainder go into training)
@@ -24,9 +26,9 @@ features_train, features_test, labels_train, labels_test = train_test_split(word
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, stop_words='english')
+
 features_train = vectorizer.fit_transform(features_train)
 features_test  = vectorizer.transform(features_test).toarray()
-
 
 ### a classic way to overfit is to use a small number
 ### of data points and a large number of features;
@@ -34,9 +36,13 @@ features_test  = vectorizer.transform(features_test).toarray()
 features_train = features_train[:150].toarray()
 labels_train   = labels_train[:150]
 
-
-
 ### your code goes here
-
-
-
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print(accuracy_score(labels_test,pred))
+print(numpy.where(clf.feature_importances_ == numpy.max(clf.feature_importances_)))
+print(numpy.max(clf.feature_importances_))
+print(vectorizer.get_feature_names_out(6965))
