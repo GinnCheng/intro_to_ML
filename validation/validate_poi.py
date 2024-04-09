b3bec36a73c0cfaba14926081117c15aee7d784a
ginn cheng <ginn.z.cheng@gmail.com>
2024-04-09 16:40:04 +0800

    chore: filled up validate_poi.py Description: -the main body of the script was updated as per the quizzes

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -14,6 +14,9 @@ import joblib
 import sys
 sys.path.append(os.path.abspath("../tools/"))
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
 data_dict = joblib.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
@@ -25,8 +28,15 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+### it's all yours from here forward!
+def tree_accuracy(x_train, x_test, y_train, y_test):
+    clf = DecisionTreeClassifier()
+    clf.fit(x_train, y_train)
+    pred = clf.predict(x_test)
+    print(f'The accuracy is {accuracy_score(y_test, pred)}')
 
-
-### it's all yours from here forward!  
-
-
+## accuracy of the original data
+tree_accuracy(features,features,labels,labels)
+## accuracy after split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+tree_accuracy(features_train, features_test, labels_train, labels_test)
